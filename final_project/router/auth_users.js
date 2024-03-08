6b3ef1f0c82f676ed16b85ca643329e8165b78fa
vishal Ravindran <vishalravindran.student@gmail.com>
2024-03-08 15:50:22 -0500

    updated login method

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,16 +7,56 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+
+    let alreadyRegisteredUser = users.filter((user)=> user.username === username);
+    if (alreadyRegisteredUser.length> 0){
+        return true;
+    }else{
+        return false;
+    }
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    let validUser = users.filter((user)=>{
+        return (user.username === username && user.password === password)
+    })
+
+    if (validUser.length > 0){
+        return true;
+    }else{
+        return false;
+    }
+
 }
+
+
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
-    
+    if (!username || !password){
+        return res.status(404).send("Invalid username or password!")
+    }
+
+    if (authenticatedUser(username, password)){
+        let accessToken = jwt.sign({
+            data:password
+        }, "access", {expiresIn: 60 * 60});
+
+        req.session.authorization = {
+            accessToken, username
+        }
+
+        return res.status(200).send("User logged in Succesfully")
+
+    }
+    else{
+        req.status(208).send("failed to authenticate user")
+    }
 });
 
 // Add a book review
